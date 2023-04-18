ba3e32dde8b27466bc820ff2299ce44819afb8c6
Rushikesh666 <65400301+Rushikesh666@users.noreply.github.com>
2023-04-18 14:32:05 +0530

    WIP Work in progress adding product page Test cases

--- src/test/java/org/com/appium/Baseclass.java
+++ src/test/java/org/com/appium/Baseclass.java
@@ -26,47 +26,53 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 
 	public class Baseclass {
 	
-	HomePageElements OnHomePage;
+	  HomePageElements OnHomePage;
 	public  static   AndroidDriver driver;
 	
 	
 	
 	@BeforeClass
-	public  void    BasTestMethod() throws InterruptedException, Exception
+	public  void    BaseTestMethod() throws InterruptedException, Exception
 
 	{
 	 
 	 	Properties prop= new Properties();
 					 
+	 	
+		/*         Providing the path of Properties File 			*/
 					 	FileInputStream fis= new FileInputStream(System.getProperty("user.dir")+"//src//main//java//res//prop.properties");
 					 
 					 	prop.load(fis);
 						
-						String IPADDRESS= prop.getProperty("ipAddress");
+					 	
+						/*     Extracting IP ADDRESS and  Port number provided under Properties File 	and passing it into   Class URL : Uniform ResourceLocator	as 	URL url = new URL( "http://127.0.0.1:4723" ); 	*/
+					
+					 	String IPADDRESS= prop.getProperty("ipAddress");
 				
+						 
 						String port= prop.getProperty("port");
-						//URL url = new URL( "http://127.0.0.1:4723" );
+				
+						
 						URL urlnew = new URL(IPADDRESS+":"+port);
 					 
-					 
-	 	UiAutomator2Options option =new UiAutomator2Options();
-	
- 
-
-	
-	option.setDeviceName("pixel2");
+						/*         Creating  capabilities object  which holds  Android and for Hybrid App	Information	*/
+						
+						UiAutomator2Options option =new UiAutomator2Options();
 	 
-		option.setApp(System.getProperty("user.dir") +"//src//main//java//res//General-Store.apk");
-	
-		option.setChromedriverExecutable(System.getProperty("user.dir") +"//src//main//java//res//chromedriver.exe");
-	
-	 driver= new AndroidDriver(urlnew, option);
-	 
-  OnHomePage= new HomePageElements(driver);
-  
-	System.out.println("BasTestMethod() running");
-	
-		Thread.sleep(5000);
+						
+						option.setDeviceName("pixel2"); // Device Name as pixel2
+						 
+						option.setApp(System.getProperty("user.dir") +"//src//main//java//res//General-Store.apk"); 								// providing Android App location path
+						
+						option.setChromedriverExecutable(System.getProperty("user.dir") +"//src//main//java//res//chromedriver.exe");		// providing Chrome Browser location path
+						
+						 driver= new AndroidDriver(urlnew, option); // Invoking Android App
+						 
+						 	OnHomePage= new HomePageElements(driver); // Creating Home Page Object 
+						 	System.out.println("Invoking Android App");
+						
+						
+							Thread.sleep(5000);
  
 }
 	
@@ -74,6 +80,11 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 	
 	public static List<HashMap<String,String>> getJsonData(String jsonFilePath) throws IOException
 	{
+
+		/*      Utiliy to read Json data and to provide to required Test Cases 	*/
+		
+		
+		
 		//	System.getProperty("user.dir")+"//src//test//java//testData//HompageData.json"
 		String jsonContent=FileUtils.readFileToString(new File( 	jsonFilePath),StandardCharsets.UTF_8);
 
